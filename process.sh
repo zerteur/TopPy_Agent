8fbd0641015894bb419d1026fec3f588966ca536
zerteur <liamoushy@gmail.com>
2023-07-19 16:00:33 +0200

    Update process.sh

--- process.sh
+++ process.sh
@@ -2,9 +2,8 @@
 
 # Fonction pour renommer un processus
 rename_process() {
-    local process_name="$1"
-    local pid="$2"
-    local new_name
+    local process_name=$1
+    local pid=$2
 
     read -p "Voulez-vous renommer le processus '$process_name' (PID: $pid) ? (Oui/Non) " choice
     
@@ -14,68 +13,30 @@ rename_process() {
     fi
 }
 
-# Fonction pour obtenir le nom d'un processus à partir de son PID
+# Fonction pour afficher les processus avec pagination
+display_processes() {
+    local process_list=$1
+    local page_size=$2
+    local current_page=$3
+    local total_pages=$4
+
+    # Calculer l'indice de début et de fin pour la pagination
+    local start_index=$(( (current_page - 1) * page_size + 1 ))
+    local end_index=$(( start_index + page_size - 1 ))
+
+    # Afficher les processus pour la page actuelle
+    echo "Liste des processus (Page $current_page/$total_pages) :"
+    echo "----------------------------------------------"
+    echo "$process_list" | awk -v start="$start_index" -v end="$end_index" 'NR>=start && NR<=end {print $1, $2}'
+    echo "----------------------------------------------"
+}
+
+# Fonction pour obtenir le nom du processus à partir du PID
 get_process_name() {
-    local pid="$1"
-    local process_name
-    
-    # Vérifier si le PID existe
-    if [[ -n $pid ]]; then
-        process_name=$(ps -p "$pid" -o comm= | awk '{print $1}')
-    fi
-    
+    local pid=$1
+    local process_name=$(ps -p $pid -o comm=)
     echo "$process_name"
 }
 
-# Fonction pour afficher les processus par pages
-display_processes() {
-    local process_list="$1"
-    local page_size=10
-    local total_processes=$(echo "$process_list" | wc -l)
-    local total_pages=$((total_processes / page_size))
-    local current_page=0
-    local start_index=0
-    
-    while true; do
-        clear
-        echo "Liste des processus (Page $((current_page + 1))/$((total_pages + 1))):"
-        echo "===================="
-        echo "$process_list" | awk "NR > $start_index && NR <= $((start_index + page_size))"
-        echo "===================="
-        echo
-        
-        if ((current_page > 0)); then
-            echo "[P]recedent"
-        fi
-        
-        if ((current_page < total_pages)); then
-            echo "[S]uivant"
-        fi
-        
-        echo "[Q]uitter"
-        echo
-        
-        read -p "Choisissez une option : " option
-        
-        case $option in
-            [Pp])
-                if ((current_page > 0)); then
-                    ((current_page--))
-                    ((start_index -= page_size))
-                fi
-                ;;
-            [Ss])
-                if ((current_page < total_pages)); then
-                    ((current_page++))
-                    ((start_index += page_size))
-                fi
-                ;;
-            [Qq])
-                break
-                ;;
-            *)
-                echo "Option invalide. Veuillez choisir une option valide."
-                ;;
-        esac
-    done
-}
+# Déclaration du tableau des process_names
+declare -A process_names
