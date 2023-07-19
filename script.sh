76c845f6b2a508b5538365a35952a2dce3a8631b
zerteur <liamoushy@gmail.com>
2023-07-19 15:24:41 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -13,47 +13,50 @@ fi
 # Installer les dépendances
 sudo pip install -r requirements.txt
 
-# Obtenir la liste des processus en cours d'exécution avec leurs PIDs
-process_list=$(ps -e -o pid,comm=)
-
-# Déclaration du tableau des process_names
-declare -A process_names
-
-# Parcourir chaque ligne du résultat de ps
-while IFS= read -r line; do
-    # Extraire le PID et le nom du processus
-    pid=$(echo "$line" | awk '{print $1}')
-    process_name=$(echo "$line" | awk '{print $2}')
+# Fonction pour sélectionner et renommer les processus
+select_and_rename_processes() {
+    # Déclaration du tableau des process_names
+    declare -A process_names
     
-    # Demander à l'utilisateur de renommer le processus
-    read -p "Voulez-vous renommer le processus '$process_name' (PID: $pid) ? (Oui/Non) " choice
+    while true; do
+        # Obtenir la liste des processus en cours d'exécution avec leurs PIDs
+        process_list=$(ps -e -o pid,comm=)
+        
+        # Parcourir chaque ligne du résultat de ps
+        while IFS= read -r line; do
+            # Extraire le PID et le nom du processus
+            pid=$(echo "$line" | awk '{print $1}')
+            process_name=$(echo "$line" | awk '{print $2}')
+            
+            # Demander à l'utilisateur de renommer le processus
+            read -p "Voulez-vous renommer le processus '$process_name' (PID: $pid) ? (Oui/Non) " choice
+            
+            if [[ $choice =~ ^[Oo]$ ]]; then
+                read -p "Entrez le nouveau nom pour le processus : " new_name
+                process_names["$process_name"]=$new_name
+            fi
+        done <<< "$process_list"
+        
+        # Proposer deux choix supplémentaires
+        read -p "Choisissez une option : [C]ontinuer à ajouter des processus, [P]asser à l'étape suivante du script : " option
+        
+        if [[ $option =~ ^[Pp]$ ]]; then
+            break
+        fi
+    done
     
-    if [[ $choice =~ ^[Oo]$ ]]; then
-        read -p "Entrez le nouveau nom pour le processus : " new_name
-        process_names["$process_name"]=$new_name
-    fi
+    # Mettre à jour le fichier config.yaml avec les process_names renommés
+    echo "process_names:" > config.yaml
     
-    # Proposer deux choix supplémentaires
-    read -p "Choisissez une option : [C]ontinuer à ajouter des processus, [P]asser à l'étape suivante du script : " option
-    
-    if [[ $option =~ ^[Cc]$ ]]; then
-        continue
-    elif [[ $option =~ ^[Pp]$ ]]; then
-        break
-    fi
-done <<< "$process_list"
+    for process_name in "${!process_names[@]}"; do
+        rename="${process_names[$process_name]}"
+        echo "  - name: \"$process_name\"" >> config.yaml
+        echo "    rename: \"$rename\"" >> config.yaml
+    done
+}
 
-# Faire une pause pour permettre à l'utilisateur de voir les résultats avant de poursuivre
-read -p "Appuyez sur Entrée pour continuer..."
-
-# Mettre à jour le fichier config.yaml avec les process_names renommés
-echo "process_names:" > config.yaml
-
-for process_name in "${!process_names[@]}"; do
-    rename="${process_names[$process_name]}"
-    echo "  - name: \"$process_name\"" >> config.yaml
-    echo "    rename: \"$rename\"" >> config.yaml
-done
+# Appeler la fonction pour sélectionner et renommer les processus
+select_and_rename_processes
 
 # Faire une pause pour permettre à l'utilisateur de voir les résultats avant de poursuivre
 read -p "Appuyez sur Entrée pour continuer..."
