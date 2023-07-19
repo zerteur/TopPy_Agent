c199a358d1a72d3bb731571d7db2c4f050e17ceb
zerteur <liamoushy@gmail.com>
2023-07-19 14:00:15 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,11 +8,12 @@ nano config.yaml
 
 # Vérifier si Python est disponible
 if command -v python &> /dev/null; then
-    # Exécuter main.py
-    python main.py
+    # Exécuter main.py en arrière-plan
+    nohup python main.py > /dev/null 2>&1 &
 elif command -v python3 &> /dev/null; then
-    # Exécuter main.py avec Python 3
-    python3 main.py &
+    # Exécuter main.py avec Python 3 en arrière-plan
+    nohup python3 main.py > /dev/null 2>&1 &
 else
     echo "Erreur : Python n'est pas installé sur ce système."
 fi
+
