50177c6b22edf1a3e1e9e764f11930af1c938e7a
zerteur <liamoushy@gmail.com>
2023-07-19 14:10:19 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Vérifier si Python est disponible
+if ! command -v python &> /dev/null && ! command -v python3 &> /dev/null; then
+    echo "Python n'est pas détecté sur ce système. Installation de Python..."
+    sudo apt-get update
+    sudo apt-get install -y python3
+fi
+
 # Installer les dépendances
 sudo pip install -r requirements.txt
 
@@ -9,11 +16,16 @@ nano config.yaml
 # Vérifier si Python est disponible
 if command -v python &> /dev/null; then
     # Exécuter main.py en arrière-plan
-    nohup python main.py > /dev/null 2>&1 &
+    nohup python main.py > $(pwd)/output.log 2>&1 &
 elif command -v python3 &> /dev/null; then
     # Exécuter main.py avec Python 3 en arrière-plan
-    nohup python3 main.py > /dev/null 2>&1 &
+    nohup python3 main.py > $(pwd)/output.log 2>&1 &
 else
     echo "Erreur : Python n'est pas installé sur ce système."
 fi
 
+# Obtenir le chemin absolu de main.py
+main_py_path=$(realpath main.py)
+
+# Ajouter main.py à la liste des tâches cron
+(crontab -l 2>/dev/null; echo "@reboot python $main_py_path > $(pwd)/output.log 2>&1") | crontab -
