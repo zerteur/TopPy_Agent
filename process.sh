ef4d72256edc373c69746430da0ffe29cbff462f
zerteur <liamoushy@gmail.com>
2023-07-19 16:17:36 +0200

    Update process.sh

--- process.sh
+++ process.sh
@@ -39,61 +39,64 @@ get_process_name() {
     echo "$process_name"
 }
 
-# Déclaration du tableau des process_names
-declare -A process_names
+# Fonction pour choisir et renommer les processus
+choose_and_rename_processes() {
+    # Déclaration du tableau des process_names
+    declare -A process_names
 
-# Boucle de choix pour ajouter/renommer des processus
-choice=""
-while [[ $choice != "P" ]]; do
-    # Obtenir la liste des processus en cours d'exécution avec leurs PIDs
-    process_list=$(ps -e -o pid,comm=)
+    # Boucle de choix pour ajouter/renommer des processus
+    choice=""
+    while [[ $choice != "P" ]]; do
+        # Obtenir la liste des processus en cours d'exécution avec leurs PIDs
+        process_list=$(ps -e -o pid,comm=)
 
-    # Paramètres pour la pagination des processus
-    page_size=10
-    current_page=1
-    total_pages=$(( ( $(ps -e -o pid= | wc -l) - 1) / page_size + 1))
+        # Paramètres pour la pagination des processus
+        page_size=10
+        current_page=1
+        total_pages=$(( ( $(ps -e -o pid= | wc -l) - 1) / page_size + 1))
 
-    # Boucle pour afficher les processus par pages
-    while true; do
-        display_processes "$process_list" $page_size $current_page $total_pages
+        # Boucle pour afficher les processus par pages
+        while true; do
+            display_processes "$process_list" $page_size $current_page $total_pages
 
-        # Proposer les options : [C]hoix, [P]récedent, [S]uivant, [Q]uitter
-        read -p "Choisissez une option : [C]hoix, [P]récedent, [S]uivant, [Q]uitter : " option
+            # Proposer les options : [C]hoix, [P]récedent, [S]uivant, [Q]uitter
+            read -p "Choisissez une option : [C]hoix, [P]récedent, [S]uivant, [Q]uitter : " option
 
-        case $option in
-            C)
-                # Choix d'un processus
-                read -p "Entrez le PID du processus que vous souhaitez renommer : " pid
-                process_name=$(get_process_name "$pid")
-                rename_process "$process_name" "$pid"
-                ;;
-            P)
-                # Page précédente
-                if (( current_page > 1 )); then
-                    (( current_page-- ))
-                fi
-                ;;
-            S)
-                # Page suivante
-                if (( current_page < total_pages )); then
-                    (( current_page++ ))
-                fi
-                ;;
-            Q)
-                # Quitter
-                break 2
-                ;;
-            *)
-                echo "Option invalide. Veuillez réessayer."
-                ;;
-        esac
+            case $option in
+                C)
+                    # Choix d'un processus
+                    read -p "Entrez le PID du processus que vous souhaitez renommer : " pid
+                    process_name=$(get_process_name "$pid")
+                    rename_process "$process_name" "$pid"
+                    ;;
+                P)
+                    # Page précédente
+                    if (( current_page > 1 )); then
+                        (( current_page-- ))
+                    fi
+                    ;;
+                S)
+                    # Page suivante
+                    if (( current_page < total_pages )); then
+                        (( current_page++ ))
+                    fi
+                    ;;
+                Q)
+                    # Quitter
+                    break 2
+                    ;;
+                *)
+                    echo "Option invalide. Veuillez réessayer."
+                    ;;
+            esac
+        done
     done
-done
 
-# Affichage des process_names renommés
-echo "Processus renommés :"
-for process_name in "${!process_names[@]}"; do
-    rename="${process_names[$process_name]}"
-    echo "  - name: \"$process_name\""
-    echo "    rename: \"$rename\""
-done
+    # Affichage des process_names renommés
+    echo "Processus renommés :"
+    for process_name in "${!process_names[@]}"; do
+        rename="${process_names[$process_name]}"
+        echo "  - name: \"$process_name\""
+        echo "    rename: \"$rename\""
+    done
+}
