fe82dd0fef7359ef6671cf60dcc8484c60f76d43
zerteur <liamoushy@gmail.com>
2023-07-19 13:55:32 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
+
 # Installer les dépendances
-pip install -r requirements.txt
+sudo pip install -r requirements.txt
 
 # Ouvrir config.yaml avec Nano
 nano config.yaml
 
-# Exécuter main.py
-python main.py
+# Vérifier si Python est disponible
+if command -v python &> /dev/null; then
+    # Exécuter main.py
+    python main.py
+elif command -v python3 &> /dev/null; then
+    # Exécuter main.py avec Python 3
+    python3 main.py
+else
+    echo "Erreur : Python n'est pas installé sur ce système."
+fi
