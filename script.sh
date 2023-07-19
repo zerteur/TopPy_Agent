9c635434ee8f65e860113e60b7678d8a40da8be5
zerteur <liamoushy@gmail.com>
2023-07-19 16:04:42 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Inclure le fichier process.sh
-source process.sh
-
 # Nom du processus
 process_name="TTAgent"
 
@@ -16,62 +13,61 @@ fi
 # Installer les dépendances
 sudo pip install -r requirements.txt
 
+# Inclure le fichier process.sh
+source process.sh
+
 # Obtenir la liste des processus en cours d'exécution avec leurs PIDs
 process_list=$(ps -e -o pid,comm=)
 
 # Déclaration du tableau des process_names
 declare -A process_names
 
-# Variables pour la pagination
-page_size=10
-current_page=1
-total_pages=$(( ($(echo "$process_list" | wc -l) - 1) / $page_size + 1 ))
+# Boucle de choix pour ajouter/renommer des processus
+choice=""
+while [[ $choice != "P" ]]; do
+    # Obtenir la liste des processus en cours d'exécution avec leurs PIDs
+    process_list=$(ps -e -o pid,comm=)
 
-# Afficher les processus par pages et demander à l'utilisateur de renommer
-display_processes "$process_list" $page_size $current_page $total_pages
-while true; do
-    read -p "Choisissez une option : [P]recedent, [S]uivant, [C]hoix, [Q]uitter : " option
-    
-    case $option in
-        [Pp])
-            if [[ $current_page -gt 1 ]]; then
-                current_page=$((current_page - 1))
-            fi
-            ;;
-        [Ss])
-            if [[ $current_page -lt $total_pages ]]; then
-                current_page=$((current_page + 1))
-            fi
-            ;;
-        [Cc])
-            read -p "Entrez le PID du processus que vous souhaitez renommer : " pid
-            process_name=$(get_process_name "$pid")
-            if [[ -z $process_name ]]; then
-                echo "PID invalide. Veuillez choisir un PID valide."
-                continue
-            fi
-            rename_process "$process_name" "$pid"
-            ;;
-        [Qq])
-            break
-            ;;
-        *)
-            echo "Option invalide. Veuillez choisir une option valide."
-            continue
-            ;;
-    esac
-    
-    # Afficher les processus mis à jour
-    display_processes "$process_list" $page_size $current_page $total_pages
-    
-    # Proposer deux choix supplémentaires
-    read -p "Choisissez une option : [C]ontinuer à ajouter des processus, [P]asser à l'étape suivante du script : " option
-    
-    if [[ $option =~ ^[Cc]$ ]]; then
-        continue
-    elif [[ $option =~ ^[Pp]$ ]]; then
-        break
-    fi
+    # Paramètres pour la pagination des processus
+    page_size=10
+    current_page=1
+    total_pages=$(( ( $(ps -e -o pid= | wc -l) - 1) / page_size + 1))
+
+    # Boucle pour afficher les processus par pages
+    while true; do
+        display_processes "$process_list" $page_size $current_page $total_pages
+
+        # Proposer les options : [C]hoix, [P]récedent, [S]uivant, [Q]uitter
+        read -p "Choisissez une option : [C]hoix, [P]récedent, [S]uivant, [Q]uitter : " option
+
+        case $option in
+            C)
+                # Choix d'un processus
+                read -p "Entrez le PID du processus que vous souhaitez renommer : " pid
+                process_name=$(get_process_name "$pid")
+                rename_process "$process_name" "$pid"
+                ;;
+            P)
+                # Page précédente
+                if (( current_page > 1 )); then
+                    (( current_page-- ))
+                fi
+                ;;
+            S)
+                # Page suivante
+                if (( current_page < total_pages )); then
+                    (( current_page++ ))
+                fi
+                ;;
+            Q)
+                # Quitter
+                break 2
+                ;;
+            *)
+                echo "Option invalide. Veuillez réessayer."
+                ;;
+        esac
+    done
 done
 
 # Faire une pause pour permettre à l'utilisateur de voir les résultats avant de poursuivre
@@ -85,6 +81,9 @@ for process_name in "${!process_names[@]}"; do
     echo "  - name: \"$process_name\"" >> config.yaml
     echo "    rename: \"$rename\"" >> config.yaml
 done
+
+# Faire une pause pour permettre à l'utilisateur de voir les résultats avant de poursuivre
+read -p "Appuyez sur Entrée pour continuer..."
 
 # Vérifier si Python est disponible
 if command -v python &> /dev/null; then
