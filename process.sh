6f3924c5fee29121a7c628feb7348af16c685e41
zerteur <liamoushy@gmail.com>
2023-07-20 12:10:02 +0200

    Update process.sh

--- process.sh
+++ process.sh
@@ -1,36 +1,127 @@
 #!/bin/bash
 
-# Inclure les fichiers de fonctions
-source "$(dirname "$0")/display_functions.sh"
-source "$(dirname "$0")/process_functions.sh"
-source "$(dirname "$0")/config_functions.sh"
+# Fonction pour afficher les processus avec pagination
+display_processes() {
+    local process_list=$1
+    local page_size=$2
+    local current_page=$3
+    local total_pages=$4
 
-# Récupérer les informations personnalisées du script principal (script.sh)
-TOKEN="$1"
-ORG="$2"
-URL="$3"
-BUCKET="$4"
+    # Calculer l'indice de début et de fin pour la pagination
+    local start_index=$(( (current_page - 1) * page_size + 1 ))
+    local end_index=$(( start_index + page_size - 1 ))
 
-# Obtenir la liste des processus en cours d'exécution avec leurs PIDs
-process_list=$(ps -e -o pid,comm=)
+    # Afficher les processus pour la page actuelle
+    clear
+    echo "Liste des processus (Page $current_page/$total_pages) :"
+    echo "----------------------------------------------"
+    echo "$process_list" | awk -v start="$start_index" -v end="$end_index" 'NR>=start && NR<=end {print $1, $2}'
+    echo "----------------------------------------------"
+}
 
-# Déclaration du tableau des process_names
-declare -A process_names
+# Fonction pour obtenir le nom du processus à partir du PID
+get_process_name() {
+    local pid=$1
+    local process_name=$(ps -p $pid -o comm=)
+    echo "$process_name"
+}
 
-# Appeler la fonction pour choisir et renommer les processus
-choose_and_rename_processes "$process_list" process_names
+# Fonction pour choisir et renommer les processus
+choose_and_rename_processes() {
+    local process_list=$1
+    local -n process_names_ref=$2
 
-# Créer le fichier config.yaml avec les informations personnalisées et les processus renommés
-create_config_yaml "$TOKEN" "$ORG" "$URL" "$BUCKET"
+    # Déclaration du tableau des process_names
+    declare -A process_names
 
-# Afficher les processus renommés
-echo "Processus renommés :"
-for pid in "${!process_names[@]}"; do
-    process_name=$(get_process_name "$pid")
-    rename="${process_names[$pid]}"
-    echo "- name: \"$process_name\""
-    echo "  rename: \"$rename\""
-done
+    # Paramètres pour la pagination des processus
+    local page_size=10
+    local current_page=1
+    local total_pages=$(( ( $(ps -e -o pid= | wc -l) - 1) / page_size + 1))
 
-# Faire une pause avant la fin du script
-read -p "Appuyez sur Entrée pour quitter le script..."
+    # Boucle pour afficher les processus par pages
+    while true; do
+        # Afficher les processus pour la page actuelle
+        display_processes "$process_list" $page_size $current_page $total_pages
+
+        # Proposer les options : [C]hoix, [P]récedent, [S]uivant, [Q]uitter
+        read -p "Choisissez une option : [C]hoix, [P]récedent, [S]uivant, [Q]uitter : " option
+
+        case $option in
+            C)
+                # Choix d'un processus
+                read -p "Entrez le PID du processus que vous souhaitez renommer : " pid
+                process_name=$(get_process_name "$pid")
+
+                read -p "Voulez-vous renommer le processus '$process_name' (PID: $pid) ? (O/N) " choice
+                if [[ $choice =~ ^[Oo]$ ]]; then
+                    read -p "Entrez le nouveau nom pour le processus : " new_name
+                    process_names["$pid"]=$new_name
+                    echo "Le processus '$process_name' (PID: $pid) a été renommé en '$new_name'."
+                else
+                    echo "Le processus '$process_name' (PID: $pid) ne sera pas renommé."
+                fi
+                ;;
+
+            P)
+                # Page précédente
+                if (( current_page > 1 )); then
+                    (( current_page-- ))
+                fi
+                ;;
+            S)
+                # Page suivante
+                if (( current_page < total_pages )); then
+                    (( current_page++ ))
+                fi
+                ;;
+            Q)
+                # Quitter
+                break
+                ;;
+            *)
+                echo "Option invalide. Veuillez réessayer."
+                ;;
+        esac
+    done
+
+    # Mettre à jour le fichier config.yaml avec les processus sélectionnés et renommés
+    local config_file="config.yaml"
+    local process_names_section="process_names:"
+    local temp_file=$(mktemp)
+
+    # Vérifier si le fichier config.yaml existe
+    if [ -f "$config_file" ]; then
+        # Copier le fichier config.yaml vers un fichier temporaire
+        cp "$config_file" "$temp_file"
+    else
+        # Créer un nouveau fichier temporaire avec la section process_names
+        echo "$process_names_section" > "$temp_file"
+    fi
+
+    # Ajouter les processus sélectionnés et renommés au fichier temporaire
+    for pid in "${!process_names[@]}"; do
+        process_name=$(get_process_name "$pid")
+        rename="${process_names[$pid]}"
+        echo "  - name: \"$process_name\"" >> "$temp_file"
+        echo "    rename: \"$rename\"" >> "$temp_file"
+    done
+
+    # Concaténer le fichier temporaire avec le fichier config.yaml
+    cat "$temp_file" >> "$config_file"
+
+    # Supprimer le fichier temporaire
+    rm "$temp_file"
+
+    # Affichage des processus renommés
+    echo "Processus renommés :"
+    for pid in "${!process_names[@]}"; do
+        process_name=$(get_process_name "$pid")
+        rename="${process_names[$pid]}"
+        echo "- name: \"$process_name\""
+        echo "  rename: \"$rename\""
+    done
+
+    # Retourner le tableau des process_names renommés
+    process_names_ref=("${process_names[@]}")
+}
